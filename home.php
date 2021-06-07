ebb9e1d8f1dcc3b867bfebe4078f23f088e12f77
ikhwanand <you@example.com>
2021-06-07 23:09:43 +0700

    Tubes basisdata

---
+++ home.php
@@ -0,0 +1,5 @@
+<p></p>
+<body>
+    <center><font size="+3" face="arial">Sistem Informasi Perpustakaan</font></center>
+    <center><font size="+3" face="arial">Perpustakaan Daerah</font></center>
+</body>
